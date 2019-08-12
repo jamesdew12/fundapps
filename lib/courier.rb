2188ed2c23ae622404d3565f4bada89622f49a26
James de Winton <james.dewinton@gmail.com>
2019-08-12 16:57:24 +0100

    final

--- lib/courier.rb
+++ lib/courier.rb
@@ -1,7 +1,9 @@
 class Courier
-  attr_reader :total_cost
+  attr_reader :total_cost, :array
   def initialize
     @total_cost = 0
+    @array = []
+    @array_for_discounts
   end
 
   def delivery_cost_size(dimensions, weight)
@@ -11,6 +13,7 @@ class Courier
             weight_over = weight - 1
             parcel += weight_over*2
           end
+      @array << "Small Parcel £#{parcel}"
         @total_cost += parcel
     elsif dimensions < 50 && dimensions > 9
         parcel = 8
@@ -18,6 +21,7 @@ class Courier
             weight_over = weight - 3
             parcel += weight_over*2
           end
+        @array << "Medium Parcel £#{parcel}"
         @total_cost += parcel
     elsif dimensions < 99 && dimensions > 49
         parcel = 15
@@ -25,6 +29,7 @@ class Courier
             weight_over = weight - 6
             parcel += weight_over*2
           end
+        @array << "Large Parcel £#{parcel}"
         @total_cost += parcel
     elsif dimensions > 99
         parcel = 25
@@ -32,11 +37,13 @@ class Courier
           weight_over = weight - 10
           parcel += weight_over*2
         end
+      @array << "XL Parcel £#{parcel}"
       @total_cost += parcel
     elsif weight > 50
         parcel = 50
         weight_over = weight - 50
         parcel += weight_over
+      @array << "Heavy Parcel £#{parcel}"
       @total_cost += parcel
     end
     end
@@ -44,5 +51,13 @@ class Courier
 
   def speedy_shipping
     @total_cost += @total_cost
+  end
+
+  def checkout
+    @array.each {|item|
+      puts item
+    }
+    puts "Total cost is £#{@total_cost}"
+
   end
 end
--- spec/courier_spec.rb
+++ spec/courier_spec.rb
@@ -7,12 +7,19 @@ describe Courier do
     subject.delivery_cost_size(7, 33.45)
     expect(subject.total_cost).to eq 85.9
   end
-  it "adds to total cost correctly" do
+  it "adds to total cost correctly and speedy shipping doubles cost" do
     subject.delivery_cost_size(71, 0.4)
     subject.delivery_cost_size(7, 0.4)
     subject.delivery_cost_size(7, 33.45)
     subject.speedy_shipping()
     expect(subject.total_cost).to eq 171.8
+  end
+  it "adds to total cost correctly and speedy shipping doubles cost" do
+    subject.delivery_cost_size(71, 0.4)
+    subject.delivery_cost_size(7, 0.4)
+    subject.delivery_cost_size(7, 33.45)
+    subject.speedy_shipping()
+    expect { subject.checkout }.to output("Large Parcel £15\nSmall Parcel £3\nSmall Parcel £67.9\nTotal cost is £171.8\n").to_stdout
   end
 
 
